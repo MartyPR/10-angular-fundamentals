2a713b3dd185b12a7d5d2fb7a55801b1b4d1618c
Marty Parada <martin.p1131@gmail.com>
2025-01-20 19:30:58 -0500

    feat(main) fix scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { MainSectionComponent } from './pages/home/main-section/main-section.component';
@@ -13,15 +12,4 @@ import { FooterComponent } from "./components/footer/footer.component";
 })
 export class AppComponent {
   title = 'pipepage';
-=======
-import { HeaderComponent } from './components/header/header.component';
-
-@Component({
-  selector: 'app-root',
-  imports: [HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-})
-export class AppComponent {
->>>>>>> 1bdb79270d4894d0982ab3287b881b3ae77b3143
 }
