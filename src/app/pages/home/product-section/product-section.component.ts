279d0b5332d587ada1341700fed518428245b4ea
Marty Parada <martin.p1131@gmail.com>
2025-01-20 19:28:09 -0500

    feat(main) add component product item

--- src/app/pages/home/product-section/product-section.component.ts
+++ src/app/pages/home/product-section/product-section.component.ts
@@ -5,41 +5,48 @@ import { ProductItemComponent } from '../../../components/product-item/product-i
   selector: 'app-product-section',
   imports: [ProductItemComponent],
   templateUrl: './product-section.component.html',
-  styleUrl: './product-section.component.scss'
+  styleUrl: './product-section.component.scss',
 })
 export class ProductSectionComponent {
-
   products = [
     {
       classOpen: 'product__item--open',
       productName: 'Capital',
-      productDescription: 'Give your customers streamlined access to capital without ever leaving your platform.',
-      productDescriptionHide: 'Pipe makes working capital a seamless, native experience for your customers.',
-      productImage: 'images/product-selector-capital-desktop.webp'
+      productDescription:
+        'Give your customers streamlined access to capital without ever leaving your platform.',
+      productDescriptionHide:
+        'Pipe makes working capital a seamless, native experience for your customers.',
+      productImage: 'images/product-selector-capital-desktop.webp',
     },
     {
       classOpen: 'product__item',
       productName: 'Business Card',
-      productDescription: 'Give your customers access to Pipe\'s frictionless card issuing, embedded natively inside your platform.',
-      productDescriptionHide: 'Launch a branded business card offering to help your customers access working capital and optimize spend management.',
-      productImage: 'images/product-selector-card-desktop.webp'
-    }]
+      productDescription:
+        "Give your customers access to Pipe's frictionless card issuing, embedded natively inside your platform.",
+      productDescriptionHide:
+        'Launch a branded business card offering to help your customers access working capital and optimize spend management.',
+      productImage: 'images/product-selector-card-desktop.webp',
+    },
+  ];
 
-    onProductHover(isHovered: boolean) {
-      console.log('Product hover state:', isHovered);
-    }
+  onProductHover(isHovered: boolean) {
+    console.log('Product hover state:', isHovered);
+  }
 
+  public classOpenCapital: string = 'product__item';
+  public classOpenBusinnessCard: string = 'product__item--open';
 
-public classOpenCapital: string = 'product__item';
-public classOpenBusinnessCard: string = 'product__item--open';
-
-toggleproductCapital(visible: boolean) {
+  toggleproductCapital(visible: boolean) {
     this.classOpenCapital = visible ? 'product__item--open' : 'product__item';
-  this.classOpenBusinnessCard = visible ? 'product__item' : 'product__item--open';
+    this.classOpenBusinnessCard = visible
+      ? 'product__item'
+      : 'product__item--open';
   }
 
   toggleproductBusinnessCard(visible: boolean) {
-    this.classOpenBusinnessCard = visible ? 'product__item--open' : 'product__item';
+    this.classOpenBusinnessCard = visible
+      ? 'product__item--open'
+      : 'product__item';
     this.classOpenCapital = visible ? 'product__item' : 'product__item--open';
-  } 
+  }
 }
